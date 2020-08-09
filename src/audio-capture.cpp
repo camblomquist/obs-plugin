a333b4cfe6266ee0afeb4323f8ad5b604896c68f
Cameron Blomquist <cameron@blomqu.ist>
2020-08-09 14:29:06 -0400

    Add additional required properties
    Eventually, we will need the hook rate and anti-cheat hook properties.
    They have been added now because I'm averse to work at the current
    moment. As an aside, fuck C++. Awful language. Even worse than Java.
    At least with C it lets you do what you want even if it's objectively
    bad to do. At least Java is consistent with its "I know better than you"
    attitude. But C++ wants to eat its cake and have it too. It decides that
    the programmer is incompetent with the small stuff and an expert where
    it really matters. Fucking enum classes. Java's enums are actually good,
    but C++ decides to have all of the restrictions of class-based enums and
    none of the benefits. And if I want to work around that, I get whining
    from the compiler. Doesn't matter that I have no control over the enums
    that have already been created and that the compiler knows it, I have to
    be told "you should really try to use our fancy pants enum classes over
    those lame C-style ones." Even though the enums are coming from C, where
    the concept of enum classes does not exist, it decides it knows better.

--- src/audio-capture.cpp
+++ src/audio-capture.cpp
@@ -19,20 +19,50 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <obs-module.h>
 #include <util/dstr.hpp>
 
+#include "plugin-macros.generated.h"
 #include "helpers/audio-session-helper.hpp"
 
 #pragma region Macros
 /* clang-format off */
 #define SETTING_SESSION				"session"
+#define SETTING_ANTI_CHEAT_HOOK		"anti_cheat_hook"
+#define SETTING_HOOK_RATE			"hook_rate"
 
 #define TEXT_AUDIO_CAPTURE			obs_module_text("AudioCapture")
 #define TEXT_SESSION				obs_module_text("AudioCapture.Session")
+#define TEXT_ANTI_CHEAT_HOOK		obs_module_text("AudioCapture.AntiCheatHook")
+#define TEXT_HOOK_RATE				obs_module_text("AudioCapture.HookRate")
+#define TEXT_HOOK_RATE_SLOW			obs_module_text("AudioCapture.HookRate.Slow")
+#define TEXT_HOOK_RATE_NORMAL		obs_module_text("AudioCapture.HookRate.Normal")
+#define TEXT_HOOK_RATE_FAST			obs_module_text("AudioCapture.HookRate.Fast")
+#define TEXT_HOOK_RATE_FASTEST		obs_module_text("AudioCapture.HookRate.Fastest")
 /* clang-format on */
+
+#define OBS_KSAUDIO_SPEAKER_4POINT1 \
+	(KSAUDIO_SPEAKER_SURROUND | SPEAKER_LOW_FREQUENCY)
 #pragma endregion
+
+/* Fuck C++ for having literally the worst implementation of enumerated
+ * types in any language I've ever used */
+enum class HookRate { SLOW, NORMAL, FAST, FASTEST }; 
 
 #pragma region Class Definition
 class AudioCaptureSource {
 	obs_source_t *source;
+
+	std::string session;
+	std::string session_id;
+	std::string device_id;
+
+	speaker_layout speakers;
+	audio_format format;
+	uint32_t samples_per_sec;
+
+	bool anticheat_hook;
+	HookRate hook_rate;
+
+	void Start();
+	void Stop();
 
 public:
 	AudioCaptureSource(obs_data_t *settings, obs_source_t *source);
@@ -42,16 +72,76 @@ public:
 };
 #pragma endregion
 
+#pragma region Misc Utility Functions
+// Borrowed from win-wasapi (win-wasapi.cpp)
+static speaker_layout ConvertSpeakerLayout(DWORD layout, WORD channels)
+{
+	switch (layout) {
+	case KSAUDIO_SPEAKER_2POINT1:
+		return SPEAKERS_2POINT1;
+	case KSAUDIO_SPEAKER_SURROUND:
+		return SPEAKERS_4POINT0;
+	case OBS_KSAUDIO_SPEAKER_4POINT1:
+		return SPEAKERS_4POINT1;
+	case KSAUDIO_SPEAKER_5POINT1_SURROUND:
+		return SPEAKERS_5POINT1;
+	case KSAUDIO_SPEAKER_7POINT1_SURROUND:
+		return SPEAKERS_7POINT1;
+	}
+
+	return static_cast<speaker_layout>(channels);
+}
+#pragma endregion
+
 #pragma region Class Implementation
+#pragma region Public
 AudioCaptureSource::AudioCaptureSource(obs_data_t *settings,
 				       obs_source_t *source)
 	: source(source)
 {
+	Update(settings);
 }
 
-AudioCaptureSource::~AudioCaptureSource() {}
+AudioCaptureSource::~AudioCaptureSource()
+{
+	Stop();
+}
 
-void AudioCaptureSource::Update(obs_data_t *settings) {}
+void AudioCaptureSource::Update(obs_data_t *settings)
+{
+	std::string new_session =
+		obs_data_get_string(settings, SETTING_SESSION);
+	bool reset = new_session != session;
+
+	if (reset) {
+		Stop();
+	}
+
+	session = new_session;
+	if (session.empty()) {
+		session_id = "";
+		device_id = "";
+	} else {
+		size_t delim = new_session.find("::");
+		session_id = session.substr(0, delim);
+		device_id = session.substr(delim);
+	}	
+
+	anticheat_hook = obs_data_get_bool(settings, SETTING_ANTI_CHEAT_HOOK);
+	hook_rate = static_cast<HookRate>(
+		obs_data_get_int(settings, SETTING_HOOK_RATE));
+
+	if (reset) {
+		Start();
+	}
+}
+#pragma endregion
+
+#pragma region Private
+void AudioCaptureSource::Start() {}
+
+void AudioCaptureSource::Stop() {}
+#pragma endregion
 #pragma endregion
 
 #pragma region OBS Source Info
@@ -76,7 +166,11 @@ static void UpdateAudioCaptureSource(void *data, obs_data_t *settings)
 	static_cast<AudioCaptureSource *>(data)->Update(settings);
 }
 
-static void GetAudioCaptureSourceDefaults(obs_data_t *settings) {}
+static void GetAudioCaptureSourceDefaults(obs_data_t *settings)
+{
+	obs_data_set_default_bool(settings, SETTING_ANTI_CHEAT_HOOK, true);
+	obs_data_set_default_int(settings, SETTING_HOOK_RATE, static_cast<int>(HookRate::NORMAL));
+}
 
 static obs_properties_t *GetAudioCaptureSourceProperties(void *data)
 {
@@ -92,12 +186,33 @@ static obs_properties_t *GetAudioCaptureSourceProperties(void *data)
 
 	for (auto session : sessions) {
 		DStr desc;
+		DStr id;
+
+		if (session.session_name.empty()) {
+			session.session_name = "Unnamed Session";
+		}
+
 		dstr_printf(desc, "[%s]: %s (%s)", session.exe.c_str(),
 			    session.session_name.c_str(),
 			    session.device_name.c_str());
-		obs_property_list_add_string(p, desc,
-					     session.session_id.c_str());
+		dstr_printf(id, "%s::%s", session.device_id.c_str(),
+			    session.session_id.c_str());
+		obs_property_list_add_string(p, desc, id);
 	}
+
+	p = obs_properties_add_bool(props, SETTING_ANTI_CHEAT_HOOK,
+				    TEXT_ANTI_CHEAT_HOOK);
+
+	p = obs_properties_add_list(props, SETTING_HOOK_RATE, TEXT_HOOK_RATE,
+				    OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_INT);
+	obs_property_list_add_int(p, TEXT_HOOK_RATE_SLOW,
+				  static_cast<int>(HookRate::SLOW));
+	obs_property_list_add_int(p, TEXT_HOOK_RATE_NORMAL,
+				  static_cast<int>(HookRate::NORMAL));
+	obs_property_list_add_int(p, TEXT_HOOK_RATE_FAST,
+				  static_cast<int>(HookRate::FAST));
+	obs_property_list_add_int(p, TEXT_HOOK_RATE_FASTEST,
+				  static_cast<int>(HookRate::FASTEST));
 
 	return props;
 }
