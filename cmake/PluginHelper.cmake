4597fd7d64c0b718f1f520d4897ecb6febad79c9
Cameron Blomquist <cameron@blomqu.ist>
2020-08-10 21:26:07 -0400

    Implement PluginHelper.cmake
    ObsPluginHelpers.cmake is included with LibObs, but visual inspection
    leads me to believe it's not going to work. The needed functionality
    from ObsPluginHelpers has been implemented in PluginHelper.
    
    Important to note that install_plugin_bin_to_data does not include the
    pdb file. It probably should, but the debug files probably wouldn't be
    helpful either because of the nature of the expected data.
    
    Since PluginHelper isn't really external, it was put into the cmake
    directory. FindLibObs has also been placed there just to keep tidy.

---
+++ cmake/PluginHelper.cmake
@@ -0,0 +1,95 @@
+# Modeled after ObsPluginHelpers, but actually useful
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	set(ARCH_NAME "64bit")
+else()
+	set(ARCH_NAME "32bit")
+endif()
+
+find_file(OBS_BUILDDIR
+    NAMES "build" "build64" "build32"
+    HINTS
+		ENV obsPath${_lib_suffix}
+		ENV obsPath
+		${obsPath}
+        "${LIBOBS_INCLUDE_DIR}/../"
+    REQUIRED
+)
+
+# The "release" folder has a structure similar OBS' one on Windows
+set(RELEASE_DIR "${CMAKE_SOURCE_DIR}/release")
+
+function(install_plugin target)
+	# Probably pointless?
+	set_target_properties(${target} PROPERTIES PREFIX "")
+	add_custom_command(TARGET ${target} POST_BUILD
+		# If config is Release or RelWithDebInfo, package release files
+		COMMAND if $<CONFIG:Release>==1 OR $<CONFIG:RelWithDebInfo>==1 (
+			"${CMAKE_COMMAND}" -E copy
+				"$<TARGET_FILE:${target}>"
+				"${RELEASE_DIR}/obs-plugins/${ARCH_NAME}"
+		VERBATIM)
+		
+		# If config is RelWithDebInfo, copy the pdb file
+		COMMAND if $<CONFIG:RelWithDebInfo>==1 (
+			"${CMAKE_COMMAND}" -E copy
+				"$<TARGET_PDB_FILE:${target}>"
+				"${RELEASE_DIR}/obs-plugins/${ARCH_NAME}"
+		VERBATIM)
+		
+		# Copy to obs-studio dev environment for immediate testing
+		COMMAND if $<CONFIG:Debug>==1 (
+			"${CMAKE_COMMAND}" -E copy
+				"$<TARGET_FILE:${target}>"
+				"$<TARGET_PDB_FILE:${target}>"
+				"${OBS_BUILDDIR}/rundir/$<CONFIG>/obs-plugins/${ARCH_NAME}"
+		VERBATIM)
+	)
+endfunction()
+
+function(install_plugin_data target data)
+	add_custom_command(TARGET ${target} POST_BUILD
+		COMMAND if $<CONFIG:Release>==1 OR $<CONFIG:RelWithDebInfo>==1 (
+			"${CMAKE_COMMAND}" -E copy_directory
+				"${CMAKE_CURRENT_SOURCE_DIR}/${data}"
+				"${RELEASE_DIR}/data/obs-plugins/${target}"
+		VERBATIM)
+	
+		COMMAND if $<CONFIG:Debug>==1 (
+			"${CMAKE_COMMAND}" -E copy_directory
+				"${CMAKE_CURRENT_SOURCE_DIR}/${data}"
+				"${OBS_BUILDDIR}/rundir/$<CONFIG>/data/obs-plugins/${target}"
+		VERBATIM)
+	)
+endfunction()
+
+function(install_plugin_with_data target data)
+	install_plugin(${target})
+	install_plugin_data(${target} ${data})
+endfunction()
+
+function(install_plugin_bin_to_data target additional_target)
+	add_custom_command(TARGET ${additional_target} POST_BUILD
+		COMMAND if $<CONFIG:Release>==1 OR $<CONFIG:RelWithDebInfo>==1 (
+			"${CMAKE_COMMAND}" -E make_directory
+				"${RELEASE_DIR}/data/obs-plugins/${target}"
+		VERBATIM)
+		
+		COMMAND if $<CONFIG:Release>==1 OR $<CONFIG:RelWithDebInfo>==1 (
+			"${CMAKE_COMMAND}" -E copy
+				"$<TARGET_FILE:${additional_target}>"
+				"${RELEASE_DIR}/data/obs-plugins/${target}"
+		VERBATIM)
+		
+		COMMAND if $<CONFIG:Debug>==1 (
+			"${CMAKE_COMMAND}" -E make_directory
+				"${OBS_BUILDDIR}/rundir/$<CONFIG>/data/obs-plugins/${target}"
+		VERBATIM)
+		
+		COMMAND if $<CONFIG:Debug>==1 (
+			"${CMAKE_COMMAND}" -E copy
+				"$<TARGET_FILE:${additional_target}>"
+				"${OBS_BUILDDIR}/rundir/$<CONFIG>/data/obs-plugins/${target}"
+		VERBATIM)
+	)
+endfunction()
